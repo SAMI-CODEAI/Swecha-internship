83ca7415d97ec2d0540bca7df130331734ac070a
Samiksha220577 <145250111+Samiksha220577@users.noreply.github.com>
2024-08-23 00:31:17 +0530

    23-8
    23-8-24-1

--- DEMO-2/chatbot/src/App.js
+++ DEMO-2/chatbot/src/App.js
@@ -1,27 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import Sample from './pages/Sample';
+import Navbar from './components/Navbar';
 
-import Home from'./pages/Home'
-import Chat from'./pages/Chat'
-import Sample from'./pages/Sample'
-import Navbar from'./components/Navbar'
 function App() {
   return (
-   <div>
-    <BrowserRouter>
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/chat" element={<Chat/>}/>
-        <Route path="/sample" element={<Sample/>}/>
-
-
-      </Routes> 
-    </BrowserRouter>
-
-   </div>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/sample" element={<Sample />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- DEMO-2/chatbot/src/pages/Chat.jsx
+++ DEMO-2/chatbot/src/pages/Chat.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import {useState} from 'react'
 function Chat() {
+    const [messages, setmessages] = useState([])
+    const [input, setinput] = useState('')
+
+    const handleSubmit=(e)=>{
+      e.preventDefault()
+      if (input.trim()){
+        setmessages([...messages,{text:input,id:Date.now()}])
+      }
+    }
   return (
-    <div>Chat</div>
+    <div className='flex flex-col h-screen bg-gray-100'>
+      <header className='bg-blue-600 text-white p-4 shadow-md'>
+        <h1 className='text-lg font-semibold'>Chat</h1>
+      </header>
+      <main>
+        <div>
+            {messages.map((msg)=>(
+              <div>
+                {msg.text}
+              </div>
+            ))}
+        </div>
+      </main>
+      <footer>
+            <form action="">
+              <input type="text" value={input} onChange={(e)=>setinput(e.target.value)} placeholder='Type anything'/>
+              <button type='submit' className='bg-blue-600 text-white px-4 py-2 rounded-r-lg hover:bg-blue-700 focus:ring'>Send</button>
+            </form>
+      </footer>
+    </div>
   )
 }
 
